acf2ca1c1746333c1bb61aa5f0d2bcc4f525dad9
largereptile <harry@yark.org.uk>
2017-12-29 17:12:55 +0000

    script test

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+	var sound = new Howl({
+      src: ['assets/do_you_want_to_cast_a_spell.ogg']
+    });
+    
+    var body = new document.getElementById("main").addEventListener("click", function(evt){
+        sound.play();
+        console.log(evt.clientX);
+        console.log(evt.clientY);
+        });
+    };
