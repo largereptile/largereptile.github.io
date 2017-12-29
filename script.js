f81f931baf111974a8d332ef8501864446bd9a57
largereptile <harry@yark.org.uk>
2017-12-29 17:51:13 +0000

    test

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function(){
       src: ['assets/do_you_want_to_cast_a_spell.ogg']
     });
     
-    var body = document.getElementsByTagName('body')[0];
+    var body = document.getElementById('main');
 	body.addEventListener("click", function(evt){
         sound.play();
         console.log(evt.clientX);
