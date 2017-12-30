2d88af11803f935595c9545ea3845d5a9e016329
largereptile <harry@yark.org.uk>
2017-12-30 10:10:12 +0000

    github is not an ide

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 window.onload = function(){
-	var sound1 = new Howl({
+    var sound1 = new Howl({
       src: ['assets/perfect.ogg']
     });
-	var sound2 = new Howl({
+    var sound2 = new Howl({
       src: ['assets/look.ogg']
     });
 	
-	var card = "assets/zola.png";
-	var gold = "assets/zola.gif";
+    var card = "assets/zola.png";
+    var gold = "assets/zola.gif";
 	
-	var golden = 0;
+    var golden = 0;
     
     var body = document.querySelector('#main');
-	body.addEventListener("click", function(evt){
-		var x = evt.clientX, y = evt.clientY;
-		img = document.createElement("img");
-		img.className = "card";
-		if (golden) {
-			img.src = "assets/zola.gif";
-			sound1.play();
-		} else {
-			img.src = "assets/zola.png";
-			sound2.play();
-			golden++;
-		};
-		img.style.top = y - 197 + "px",
-		img.style.left = x - 143 + "px";
-		img.draggable = false,		 
-		img.ondragstart = function() {
+    body.addEventListener("click", function(evt){
+        var x = evt.clientX, y = evt.clientY;
+        img = document.createElement("img");
+        img.className = "card";
+        if (golden) {
+            img.src = "assets/zola.gif";
+            sound1.play();
+        } else {
+            img.src = "assets/zola.png";
+            sound2.play();
+            golden++;
+        };
+        img.style.top = y - 197 + "px",
+        img.style.left = x - 143 + "px";
+        img.draggable = false,		 
+        img.ondragstart = function() {
 		      return false;
-		 }
-		body.appendChild(img);
+        }
+        body.appendChild(img);
         });
     };
